2bb8f3f82473980c2f2a2600e8eb431f1020a564
ocros03 <47435203+ocros03@users.noreply.github.com>
2022-08-04 14:15:38 -0400

    Add files via upload

---
+++ Untitled-1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int age;
+
+	cout<<"Enter your age: ";
+	cin>>age;
+
+	if(age>=21)
+	{
+		cout<<"Drink"<<endl;
+	}
+	else
+	{
+		cout<<"No Drink"<<endl;
+	}
+
+	return 0;
+}
